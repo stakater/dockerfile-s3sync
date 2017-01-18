b77e36f4c669f6714d6945edfeddb7bae957b918
hazim1093 <hazim_malik@hotmail.com>
2017-01-18 17:34:17 +0500

    Update scripts

--- s3sync.sh
+++ s3sync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "$(date) - Syncing s3://${S3_BUCKET_LOCATION} with /s3sync/${S3_BUCKET_LOCATION}"
-aws s3 sync s3://$S3_BUCKET_LOCATION /s3sync/$S3_BUCKET_LOCATION --delete
+echo "$(date) - Syncing s3://${S3_BUCKET_LOCATION} with /s3sync"
+aws s3 sync s3://$S3_BUCKET_LOCATION /s3sync --delete
--- start.sh
+++ start.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+
+# First sync
+/s3sync.sh > /var/log/s3sync.log 2>&1
+
 # Run cron job every 30 seconds
 echo "* * * * * /s3sync.sh >> /var/log/s3sync.log 2>&1
 * * * * * (sleep 30; /s3sync.sh) >> /var/log/s3sync.log 2>&1" > /crontab
+
+# To make logs available at `docker logs`
+tail -f /var/log/s3sync.log &
+
+# Start cron
 /usr/bin/crontab /crontab
 /usr/sbin/crond -f -L 8
